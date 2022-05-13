f49919da02bddcae48effc2ef63fde6445bcb31f
Jason-lin1999 <765854144@qq.com>
2022-05-13 16:57:38 +0800

    go新建了两个文件，用于json转结构体，结构体转json。但还没有是实现

---
+++ go/JsonToStruct.go
@@ -0,0 +1,10 @@
+package main
+// 这个程序的目的是为了将json文件转为结构体
+
+
+  
+import (
+	"fmt"
+	"encoding/json"
+)
+
---
+++ go/StructToJason.go
@@ -0,0 +1,60 @@
+package main
+// 这个程序的目的是为了将结构体转为json文件
+
+
+  
+import (
+	"fmt"
+	"encoding/json"
+)
+  
+type Books struct {
+    Title string
+    Author string
+    Subject string
+    Book_id int
+}
+  
+// func changeBook(book Books) string {   //把book对象传进来，返回的值是string类型的，也就是将被修改的值返回出来。
+//     book.title = "book1_change"
+//     return book.title
+// }
+  
+func main() {
+    var book1 Books
+	// book1 :=Books{}
+    book1.Title = "book3"
+	book1.Subject ="math"
+    book1.Author = "zuozhe"
+    book1.Book_id = 1
+
+
+	book2 :=&book1
+	book2.Author="gaofeng1"
+
+	// book3 :=*book2
+
+	// fmt.Println(book3)
+	// data, _ := json.MarshalIndent(book3, "", "   ")
+		
+	data, _ := json.Marshal(&(*book2))
+ 
+	fmt.Println(string(data))
+
+	old_map :=make(map[string]interface{})
+	err :=json.Unmarshal([]byte(data),&old_map)
+	if err != nil {
+		fmt.Println("err=",err)
+		
+	}
+
+	for key, val := range old_map {
+		if(key=="Author"){
+			fmt.Println(val)
+		}
+		// fmt.Println("key=",key)
+		// fmt.Println("val=",val)
+	}
+
+
+}
