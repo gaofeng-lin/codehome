df4849eab2b79dad77792afa9c846dab5e5e681c
Jason-lin1999 <765854144@qq.com>
2022-05-29 16:55:32 +0800

    my-app目录。实现了从postman取jsonschema数据，界面上赋值，然后传到testgin项目，输出效果与预期一致

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -11,9 +11,7 @@ import { JSONSchema7 } from "json-schema";
 
 
 interface isState {
-  schema: JSONSchema7,
-  formData: JSON
-
+  schema: JSONSchema7
 }
 
 
@@ -34,11 +32,47 @@ const schema = {
   }
 };
 const onSubmit = ({formData} ) => {
-  console.log("Data submitted: ",  formData);
+  axios({
+    headers: {
+      "Content-Type": "application/octet-stream",
+      "Access-Control-Allow-Origin": "*",
+    },
+    url: 'http://127.0.0.1:8100/api/test',
+    method: 'POST',
+    data: formData
+  })
 }
+
 
 class App extends React.Component <any, isState> {
 
+  constructor(props:any) {
+    super(props);
+    this.state = {
+      schema:{
+        "title": "Test form",
+        "type": "object",
+        "properties": {
+          "name": {
+            "type": "string"
+          },
+          "age": {
+            "type": "number"
+          },
+          "money": {
+            "type": "string"
+          }
+        }
+      }
+    }
+
+  }
+
+  componentDidMount() {
+    axios({url:'https://63cc3e21-7c75-4507-8414-11dde227bc60.mock.pstmn.io/test'}).then((data) => {
+      this.setState({schema:data.data});
+    })
+  }
 
 
     render(){
