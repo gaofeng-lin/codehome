83e9e18df8cbc2cb452462232e9755a1f2c8d47c
Jason-lin1999 <765854144@qq.com>
2022-06-11 15:54:10 +0800

    内部bean注入

--- java/Spring/src/main/java/org/example/App.java
+++ java/Spring/src/main/java/org/example/App.java
@@ -6,6 +6,7 @@ package org.example;
  */
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.innerbean.Employee;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
@@ -13,7 +14,7 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 //        HelloWorld obj = context.getBean("helloWorld",HelloWorld.class);
-        Student obj = context.getBean("student", Student.class);
+        Employee obj = context.getBean("employee", Employee.class);
 //        obj.getMessage();
         LOGGER.info(obj.toString());
 
