444c1a22b25b609b2cdf11845405582898bff18d
Jason-lin1999 <765854144@qq.com>
2022-07-13 00:19:39 +0800

    批量添加产品

--- sql/mptt/init_table.sql
+++ sql/mptt/init_table.sql
@@ -10,9 +10,10 @@ CREATE TABLE `mptt`  (
   `node_name` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `lft` bigint(20) NOT NULL,
   `rgt` bigint(20) NOT NULL,
+  `node_level` bigint(20) NOT NULL,
   PRIMARY KEY (`node_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
 
-insert into mptt (node_name, lft, rgt) values ('root', 1,2);
+insert into mptt (node_name, lft, rgt, node_level) values ('root', 1,2,1);
--- sql/mptt/insert_node.sql
+++ sql/mptt/insert_node.sql
@@ -32,20 +32,20 @@ end $$
 /* 构造一个节点 */
 
 delimiter $$
-drop procedure if exists `insert_mptt`;
-create procedure `insert_mptt`(in nid Integer)
+drop procedure if exists insert_mptt;
+create procedure insert_mptt(in nid Integer,in p_name varchar(20),in n_level integer)
 begin
 
-declare p_name varchar(50);
+-- declare p_name varchar(50);
 
-set p_name = rand_string(4);
+-- set p_name = rand_string(4);
 
 select @nrgt:= rgt from mptt where node_id = nid ;
 update mptt set rgt = rgt + 2 where rgt >= @nrgt ;
 update mptt set lft = lft + 2 where lft >= @nrgt ;
 
 
-insert into mptt (node_name, lft, rgt) values(p_name, @nrgt, @nrgt + 1);
+insert into mptt (node_name, lft, rgt, node_level) values(p_name, @nrgt, @nrgt + 1, n_level);
 
 
 end $$
@@ -55,24 +55,70 @@ end $$
 /* 二级节点 --产品名称*/
 delimiter $$
 drop procedure if exists `two_level`;
-create procedure `two_level`(in p_name varchar, in level int)
+create procedure `two_level`(in p_name varchar(20))
 begin
 
-/* 获取一级节点最大的id */
+    /* 获取一级节点最大的id */
+    select @target_id:=MAX(node_id) from mptt where node_level=1 ;
 
-delcare father_id int;
-
+    call insert_mptt(@target_id, p_name, 2);
+    
 
 
 end $$
 
 
-/* 三级节点 */
+/* 三级节点-- 求解器版本号，产品说明，模块（module,name）*/
+delimiter $$
+drop procedure if exists `three_level`;
+create procedure `three_level`(in p_name varchar(20))
+begin
 
-/* 四级节点 */
+    
+    select @target_id:=MAX(node_id) from mptt where node_level=2 ;
+
+    call insert_mptt(@target_id, p_name, 3);
+
+end $$
+/* 四级节点--模块 */
+delimiter $$
+drop procedure if exists `four_level`;
+create procedure `four_level`(in p_name varchar(20))
+begin
+
+    
+    select @target_id:=MAX(node_id) from mptt where node_level=3 ;
+
+    call insert_mptt(@target_id, p_name, 4);
+
+end $$
 /* 五级节点 */
 /* 六级节点 */
 
 
 
-/* call insert_mptt(2); */
+--  call insert_mptt(1,'heop'); 
+-- call two_level('风雷2号');
+
+-- 打造一个产品
+delimiter $$
+drop procedure if exists `create_product`;
+create procedure `create_product`()
+begin
+    declare i int default 0;
+    call two_level('风雷1号');
+    
+    call three_level('200');
+    call three_level('使用了算发2');
+    call three_level('模块');
+    while i < 3 do
+    call four_level(concat('模块',i));
+
+
+    set i = i + 1;
+    end while;
+    
+end $$
+
+call create_product();
+
