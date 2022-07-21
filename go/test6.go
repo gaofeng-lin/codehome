7e0bd903671778a401ba79891fd6ac2b18197200
Jason-lin1999 <765854144@qq.com>
2022-07-21 17:19:18 +0800

    golang项目一个小例子

--- go/test6.go
+++ go/test6.go
@@ -14,22 +14,12 @@ import (
 
 func main() {
 
-    s := []map[string]interface{}{}
+    versionRelationMap := map[string]string{"mgCFLScale":"cflMGTimes", "CFLEnd":"cfl_end", "CFLVaryStep":"cfl_nstep"}
 
-    m1 := map[string]interface{}{"name": "John", "age": 10}
-    m2 := map[string]interface{}{"name": "Alex", "age": 12}
-
-    s = append(s, m1, m2)
-    s = append(s, m2)
-
-    // b, err := json.Marshal(s)
-    // if err != nil {
-    //     fmt.Println("json.Marshal failed:", err)
-    //     return
-    // }
-
-
-    // fmt.Println("b:", string(b))
-	fmt.Println(s)
+    for key, val := range versionRelationMap{
+        if key == "mgCFLScale" {
+            fmt.Println(val)
+        }
+    }
 
 }
