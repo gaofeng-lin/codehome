6d2670ea88bca876eb5859b01173f331beedbbfa
Jason-lin1999 <765854144@qq.com>
2022-07-23 10:24:09 +0800

    保存下

---
+++ go/typeJudge.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	// "crypto/ecdsa"
+	"fmt"
+	// "reflect"
+	// "strings"
+	// "strconv"
+	// "sort"
+	"reflect"
+)
+
+func main() {
+	 x  := 66.99
+
+	// val := reflect.TypeOf(x)
+	
+	// fmt.Println(val)
+
+	switch x.(type){
+		case int:
+
+	}
+}
