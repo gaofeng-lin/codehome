58065038030e349bb6db18c1402826171c76b343
Jason-lin1999 <765854144@qq.com>
2022-07-25 23:59:09 +0800

    ts练习

--- test-typescript/src/App.tsx
+++ test-typescript/src/App.tsx
@@ -3,17 +3,21 @@ import React, {useEffect, useLayoutEffect} from 'react';
 
 export default function App() {
   const [state, setState] = React.useState(0);
+  console.log("我是1.0的打印语句");
   useEffect(() => {
     console.log("第一次render时执行");
   }, []);
   useEffect(() => {
     if (state > 0) {
-      console.log("第二次之后render时执行");
+      // console.log("第二次之后render时执行");
+      console.log('第 %i 次之后render时执行',state);
     }
   }, [state]);
   useLayoutEffect(() => {
     console.log("我比useEffect先执行");
   });
+  console.log("我是2.0的打印语句");
+  console.log("我是3.0的打印语句");
   return (
     <div className="App">
       <h1>{state}</h1>
