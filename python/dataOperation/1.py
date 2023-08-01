4217e8042854b31b721691aa9ee76c137622a38c
gaofeng-lin <765854144@qq.com>
2023-08-01 17:47:04 +0800

    python用matplotlib画图

---
+++ python/dataOperation/1.py
@@ -0,0 +1,14 @@
+#coding=UTF-8
+
+import numpy as np
+
+# array = np.random.rand(3,10,1,15,32,3,32)
+
+# print(array.shape)
+# breakpoint()
+# array1 = array.transpose(0, 1, 2, 5, 4)
+
+# print(array1.shape)
+
+a = np.zeros((1, 3))
+print (a)
---
+++ python/tianci/plot_ux_one.py
@@ -0,0 +1,99 @@
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+def generate(num):
+    # f = h5py.File('./Data_16.h5', 'r')
+    f = h5py.File('C:\\Users\\76585\\Desktop\\tianchi\\data_16.h5', 'r')
+    n_frame = len(f.keys())
+    XYT = np.zeros((0,3))
+    UX = np.zeros((0,1))
+    UY = np.zeros((0,1))
+    P = np.zeros((0,1))
+    for key in f.keys():
+        time = float(key)
+        XY = f[key][:,0:2]
+        ux = f[key][:,2][:,None]
+        uy = f[key][:,3][:,None]
+        p = f[key][:,4][:,None]
+        XYT = np.concatenate((XYT, np.concatenate((XY, np.full((XY.shape[0],1), time)), axis=1)), axis=0)
+        UX = np.concatenate((UX, ux), axis=0)
+        UY = np.concatenate((UY, uy), axis=0)
+        P = np.concatenate((P, p), axis=0)
+    X, y = {}, {}
+    randXY = np.random.rand(num['pde']*n_frame, 2)*2*np.pi-np.pi
+    randT = np.random.rand(num['pde']*n_frame, 1) * 30
+    X['pde'] = np.concatenate((randXY, randT), axis=1)
+    X['data'] = XYT
+    y['data'] = np.concatenate((UX, UY, P), axis=1)
+    return X, y
+
+
+train_num = {'pde': 1024}
+
+X, y = generate(train_num)
+truth_xyt = X['data']
+# print('truch_xyt shape: ', truth_xyt.shape)
+
+ux = y['data'][:,0:1]
+uy = y['data'][:,1:2]
+p = y['data'][:,2:3]
+
+xy = truth_xyt[:, 0:2]
+
+# print('ux shape :', xy.shape)
+
+fig = plt.figure(figsize=(10, 8))
+ax = fig.add_subplot(111, projection='3d')
+
+# 初始化最大值和最小值
+zmin = np.inf
+zmax = -np.inf
+zmax_idx = None
+
+i = 0
+
+# 取256行数据，如果不足256行，则取所有剩余的行
+end_idx = min(i+256, xy.shape[0])
+
+# 提取 x, y, z 坐标
+x = xy[i:end_idx, 0]
+y = xy[i:end_idx, 1]
+z = ux[i:end_idx, 0]
+
+# 找到本次循环中的最大值和对应的索引
+local_max_idx = np.argmax(z)
+local_max = z[local_max_idx]
+
+# 如果本次循环的最大值大于当前最大值，则更新最大值和最大值索引
+if local_max > zmax:
+    zmax = local_max
+    zmax_idx = (x[local_max_idx], y[local_max_idx], zmax)
+
+# 找出最大值和最小值
+# zmin = np.min(z)
+# zmax = np.max(z)
+# 绘制散点图
+ax.scatter(x, y, z, marker='o')
+
+# 更新最大值和最小值
+zmin = min(zmin, np.min(z))
+# zmax = max(zmax, np.max(z))
+
+# 绘制最大值点，用红色表示
+ax.scatter(zmax_idx[0], zmax_idx[1], zmax_idx[2], color='red', s=100)
+
+# 将最大值和最小值添加到图中
+# ax.text(x[0], y[0], zmax, "Max Z: %.2f" % zmax, color='red')
+# ax.text(x[0], y[0], zmin, "Min Z: %.2f" % zmin, color='blue')
+
+# 设置轴的标签和标题
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('ux')
+plt.title('3D Scatter Plot\nMin ux: %.2f, Max ux: %.2f' % (zmin, zmax))
+
+# 展示图形
+plt.show()
