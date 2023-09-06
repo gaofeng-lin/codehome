db51f827908df3d23604f4ab882d23af6955f257
gaofeng-lin <765854144@qq.com>
2023-09-06 17:18:09 +0800

    数组之间相除

--- python/tianci/test.py
+++ python/tianci/test.py
@@ -1,19 +1,14 @@
-# 这个代码是取出一个时间切片，然后看看在x
-
 import numpy as np
+d = 2*np.pi/512
 
-n = 512
-bin_width = 2*np.pi/n
-x = np.linspace(-np.pi+bin_width/2, np.pi-bin_width/2, 512)
-y = np.linspace(np.pi-bin_width/2, -np.pi+bin_width/2, 512)
-X, Y = np.meshgrid(x, y)
-X = np.array(X.flatten())
-Y = np.array(Y.flatten())
-print('X: ',X)
-print("Y: " ,Y)
+x = np.linspace(-np.pi, np.pi-d, 512)
 
-# for i in range(0, 512*512):
-#     XY = np.array([X[i], Y[i]])
-#     # print(XY)
-#     print(XY)
-#     breakpoint
+y = np.linspace(-np.pi, np.pi-d, 512)
+
+coordinates = [[xi, yi] for xi in x for yi in y]
+
+coordinates1 = [[xi+1e-8, yi] for xi in x for yi in y]
+
+print(coordinates[0:10])
+print('/n')
+print(coordinates1[0:10])
